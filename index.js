165e059d49e2647cd1cff30ef3ada19ae3843356
Emiliano Vazquez Banda <aoe2_emi@hotmail.com>
2021-11-10 12:21:09 -0600

    Validations to the user input

--- index.js
+++ index.js
@@ -4,25 +4,28 @@ import showResult from "./js/showResults.js";
 
 const SEARCH_BUTTON = document.getElementById('btnSearch');
 const USER_TEXT = document.getElementById('txtUsername');
-const CONTENT_AREA = document.querySelector('.flex-container');
+
+const resetView = (message) => {
+  document.querySelector('#avatar').src = "/images/profile.png";
+  document.querySelector('.flex-container').innerHTML = `<h2>${message}</h2>`;
+}
 
 SEARCH_BUTTON.addEventListener('click', async () => {
   const COLOR = Math.floor(Math.random()*16777215).toString(16);
-  if (USER_TEXT.value.length > 0){
-    const USER = USER_TEXT.value;
+  const USER = USER_TEXT.value;
+
+  if (USER.length > 0){
     const USER_DATA = await getUserData(USER);
-    const REPOS = await getUserRepos(USER);
     
-    if (REPOS) 
-      showResult(USER_DATA, REPOS);
+    if (!(USER_DATA.message == "Not Found")){
+      const REPOS = await getUserRepos(USER);
+      (REPOS) ? showResult(USER_DATA, REPOS) : resetView("We couldn't find any repositories associated to this account");
+      document.documentElement.style.setProperty('--main-color', `#${COLOR}`);
+    } 
     else 
-      CONTENT_AREA.innerHTML = `<h2>We couldn't find any repositories associated to this account</h2>`;
-
-    document.documentElement.style.setProperty('--main-color', `#${COLOR}`);
-  } else {
-    CONTENT_AREA.innerHTML = `
-      <h2>Enter a username first</h2>
-    `
-  }
+      resetView("We couldn't find any github profile associated to this username");
+  } 
+  else 
+    resetView('Enter a username first');
 })
-
+  
