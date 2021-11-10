47833d8210748103437a36b015637f2d9a4cda02
Emiliano Vazquez Banda <aoe2_emi@hotmail.com>
2021-11-09 20:12:37 -0600

    async/await at the moment of fetch

--- js/getUserData.js
+++ js/getUserData.js
@@ -1,20 +1,14 @@
 import API from "./apiData.js";
 
-const getUserData = (user) => {
-
-  fetch(`${API.url}/users/${user}`, {
+const getUserData = async user => {
+  return await fetch(`${API.url}/users/${user}`, {
     method: 'GET',
     headers: {
       Authorization: API.key,
       accept: 'application/vnd.github.v3+json',
     }
   })
-    .then(response => response.json())
-    .then(user => {
-
-      console.log(user)
-
-    })
-  };
+  .then(response => response.json())
+};
 
 export default getUserData;
--- js/getUserRepos.js
+++ js/getUserRepos.js
@@ -1,20 +1,14 @@
 import API from "./apiData.js";
 
-const getUserRepos = (user) => {
-
-  fetch(`${API.url}/users/${user}/repos`, {
+const getUserRepos = async user => {
+  return await fetch(`${API.url}/users/${user}/repos`, {
     method: 'GET',
     headers: {
       Authorization: API.key,
       accept: 'application/vnd.github.v3+json',
     }
   })
-    .then(response => response.json())
-    .then(repos => {
-
-      console.log(repos)
-
-    })
-  };
+  .then(response => response.json())
+};
 
 export default getUserRepos;
